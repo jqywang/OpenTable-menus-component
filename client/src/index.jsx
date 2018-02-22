9f436bb439411b4bfb3378a10b264802016e1ddb
Jeffrey Wang <jqywang.95@gmail.com>
2018-02-22 12:03:42 -0800

    Skeleton now serves static files, tested react and webpack to make sure it works

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,1 +1,13 @@
-console.log('what up from index jsx');
+import React from 'react';
+import ReactDOM from 'react-dom';
+class TestApp extends React {
+    constructor(props){
+        super(props);
+    }
+    render(){
+        return (
+            <h3>Hey from Babel processed react thing</h3>
+        );
+    }
+}
+ReactDOM.render(<TestApp/>, document.findElementById('app'));
--- server/index.js
+++ server/index.js
@@ -1,9 +1,7 @@
-console.log('heythere');
-console.log('from index js server');
-var express = require(express);
+var express = require('express');
 var app = express();
 
-app.use(express.static(__dirname + '../client/dist/'));
+app.use(express.static(__dirname + '/../client/dist/'));
 app.listen(3000, function () {
-    console.log('listening on 3000');
+    console.log('listening on 3000 man');
 });
