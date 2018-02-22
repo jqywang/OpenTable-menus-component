1c46b2fc51c5e8174ac8a23f56949de32dade288
Jeffrey Wang <jqywang.95@gmail.com>
2018-02-22 10:56:29 -0800

    added bare bones server

--- server/index.js
+++ server/index.js
@@ -1,2 +1,9 @@
 console.log('heythere');
 console.log('from index js server');
+var express = require(express);
+var app = express();
+
+app.use(express.static(__dirname + '../client/dist/'));
+app.listen(3000, function () {
+    console.log('listening on 3000');
+});
