03fb6d260e81a5653c67f21f3caf0721f090d7e9
Jeffrey Wang <jqywang.95@gmail.com>
2018-02-23 10:53:14 -0800

    set up database file with skeleton, havent tested it yet. Changed format a litle bit of schema and jsonMenus fake dat

---
+++ database/menudb.js
@@ -0,0 +1,28 @@
+import { mongo } from 'mongoose';
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/menus');
+let entrySchema = mongoose.Schema({
+    menu_name: String,
+    subgroup_name: String,
+    subgroup_desc: String,
+    entry_name: String,
+    entry_desc: String,
+    entry_price: String,
+    extras_name: String,
+    extras_price: String
+});
+let Entry = mongoose.model('Entry', entrySchema);
+
+let saveIntoDB = function (entryObj) {
+    //SINGLE OBJECT
+    let entry = new Entry(entryObj);
+    entry.save(function(err, success){
+        if(err){
+            console.log(err);
+            throw err;
+        } else {
+            console.log('successfully put into the goddamn db bb');
+        }
+    });
+}
