204966135d1743ba39d454b9dfe4294eb576cd38
Jeffrey Wang <jqywang.95@gmail.com>
2018-02-23 14:43:37 -0800

    syntax errors

--- tests/db.test.js
+++ tests/db.test.js
@@ -1,25 +1,26 @@
 const mongoose = require('mongoose');
-const dbHelper = require('../database/menudb.js');
-describe('test the fact that the database is seeded', () => {
-    beforeAll(() => {
-        mongoose.connect('mongodb://localhost/entries');
-    });
-    afterAll((done) => {
-        mongoose.disconnect(done);
-    });
-    test('has fake data in database', () => {
-        dbHelper.pullFromDB((err, data) => {
-            if(err){throw err;}
-            console.log(data.length);
-            expect(data.length).toBe(58);
-        });
-    });
-    test('has a function that pulls specific menus from db by restaurant id', () => {
-        dbHelper.pullRestaurantFromDB('1', (err, data) => {
-            if(err){throw err;}
-            expect(data.length).toBe(29);
-            expect(data[0].restaurant_id).toBe('1');
-        })
-    });
-})
 
+const dbHelper = require('../database/menudb.js');
+
+describe('test the fact that the database is seeded', () => {
+  beforeAll(() => {
+    mongoose.connect('mongodb://localhost/entries');
+  });
+  afterAll((done) => {
+    mongoose.disconnect(done);
+  });
+  test('has fake data in database', () => {
+    dbHelper.pullFromDB((err, data) => {
+      if (err) { throw err; }
+      console.log(data.length);
+      expect(data.length).toBe(58);
+    });
+  });
+  test('has a function that pulls specific menus from db by restaurant id', () => {
+    dbHelper.pullRestaurantFromDB('1', (err, data) => {
+      if (err) { throw err; }
+      expect(data.length).toBe(29);
+      expect(data[0].restaurant_id).toBe('1');
+    });
+  });
+});
