c4ed945e4220f6e93dbfcc89a00da4c86bab45d4
Jeffrey Wang <jqywang.95@gmail.com>
2018-02-24 12:33:37 -0800

    rewrote tests to test for an extras object in entry obj

--- tests/restructure.test.js
+++ tests/restructure.test.js
@@ -4,6 +4,11 @@ const restructure = require('../server/dataRestructure.js');
 
 test('data restructuring helper function should return object with correct nesting', () => {
   let testData = restructure.dataRestructure(dummyData.slice(0, 4));
-  expect(testData.Dinner.Appetizers.entries.length).toBe(5);
+  expect(testData.Dinner.Appetizers.entries.length).toBe(4);
   expect(testData.Dinner.Appetizers.entries[0].name).toBe('artichoke');
+  expect(testData.Dinner.Appetizers.entries[0].desc).toBe('tasty');
+  expect(testData.Dinner.Appetizers.entries[0].price).toBe('$27.00');
+  expect(typeof testData.Dinner.Appetizers.entries[0].extras).toBe('object');
+
+  
 });
