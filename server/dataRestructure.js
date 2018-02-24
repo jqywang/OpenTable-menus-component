525fbaaa6b0f5357e8d61c68130ea81e5a336ac0
Jeffrey Wang <jqywang.95@gmail.com>
2018-02-24 12:30:58 -0800

    Wrote out helper function to restructure database data

---
+++ server/dataRestructure.js
@@ -0,0 +1,25 @@
+// restructure database array of unnested objects into array of
+// nested objects that our react components can use
+
+const dataRestructure = (objArr) => {
+  let menus = {};
+  for (let i = 0; i < objArr.length; i++) {
+    if (!menus[objArr[i].menu_name]) {
+      menus[objArr[i].menu_name] = {};
+    }
+    if (!menus[objArr[i].menu_name][objArr[i].subgroup_name]) {
+      menus[objArr[i].menu_name][objArr[i].subgroup_name] = {subgroup_desc: objArr[i].subgroup_desc, entries: [] };
+    }
+    var menuObj = menus[objArr[i].menu_name];
+    var subGroupObj = menuObj[objArr[i].subgroup_name];
+    var entryObj = {
+      name: objArr[i].entry_name,
+      price: objArr[i].entry_price,
+      desc: objArr[i].entry_desc,
+    };
+    subGroupObj.entries.push(entryObj);
+  }
+  return menus;
+};
+
+module.exports.dataRestructure = dataRestructure;
