d9aa58e3c7479bba030844a666978e35d3a87ea6
Jeffrey Wang <jqywang.95@gmail.com>
2018-02-24 10:07:00 -0800

    wrote tests for status code of server

--- tests/server.test.js
+++ tests/server.test.js
@@ -1,10 +1,17 @@
 const request = require('request');
 
 describe('server should serve correct responses', () => {
-  it('should serve 200 for successful get request', () => {
-    request.get(url, (err, response, body) => {
+  it('should serve static files for successful get request', () => {
+    request.get('http://localhost:3000/', (err, response, body) => {
       if (err) { throw err; }
-      expect(response).toBe(200);
+      expect(typeof response.body).toBe('string');
+    });
+  });
+
+  it('should give 200 for successful get request to a restaurant id url', () => {
+    request('http://localhost:3000/menus/2', (err, response) => {
+      if (err) { throw err; }
+      expect(response.statusCode).toBe(200);      
     });
   });
 });
