22a93fb88f6a54f696b6596039650856c6b3a6f5
Jeffrey Wang <jqywang.95@gmail.com>
2018-02-26 11:58:33 -0800

    wrote out some tests to make sure server was returning correct data in correct form

--- tests/server.test.js
+++ tests/server.test.js
@@ -4,7 +4,7 @@ describe('server should serve correct responses', () => {
   it('should serve static files for successful get request', () => {
     request.get('http://localhost:3000/', (err, response) => {
       if (err) { throw err; }
-      expect(typeof response.body).toBe('string');
+      expect(typeof response.body).toBe('object');
     });
   });
 
@@ -12,7 +12,23 @@ describe('server should serve correct responses', () => {
     request('http://localhost:3000/api/menus/2', (err, response) => {
       if (err) { throw err; }
       expect(response.statusCode).toBe(200);
-      expect(JSON.parse(response.body).id).toBe("2");
+      expect(JSON.parse(response.body).id).toBe('2');
+    });
+  });
+
+  it('should serve back all requested data from database', () => {
+    request.get('http://localhost:3000/api/menus/2', (err, response) => {
+      if (err) { throw err; }
+      expect(typeof response.body.Dinner).toBe('object');
+      expect(typeof response.body.Lunch).toBe('object');
+    });
+  });
+
+  it('should give back the correct data', () => {
+    request.get('http://localhost:3000/api/menus/2', (err, response) => {
+      if (err) { throw err; }
+      expect(response.body.Dinner.Appetizers.entries[0]).toBe('object');
+      expect(response.body.Dinner.Appetizers.subgroup_desc).toBe('add cheese for $1.00');
     });
   });
 });
