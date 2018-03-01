2bba4c2b4b78ab5a8df36c1959b75fe6af46227b
Jeffrey Wang <jqywang.95@gmail.com>
2018-02-28 20:55:08 -0800

    wrote out more tests for my frontend components

--- tests/component.test.js
+++ tests/component.test.js
@@ -1,26 +1,27 @@
-import MenuComponent from '../client/src/menuComponent.jsx';
-
-import EntryComponent from '../client/src/entryComponent.jsx';
-
-import SubgroupComponent from '../client/src/subgroupComponent.jsx';
-
 import React from 'react';
 
-const fake = {
-  Dinner: 'yes',
-  Lunch: 'also yes'
-};
-const wrapper = global.shallow(<MenuComponent menus = {fake}/>);
-describe('menu should show correct things', () => {
-  expect(wrapper.is('div')).toEqual(true);
-  expect(wrapper.find('button').length).toBe(2);
-});
+import MenuComponent from '../client/src/menuComponent';
+
+import EntryComponent from '../client/src/entryComponent';
+
+import SubgroupComponent from '../client/src/subgroupComponent';
 
 describe('menu buttons should change state', () => {
+  const fake = {
+    Dinner: 'yes',
+    Lunch: 'also yes'
+  };
+  const wrapper = global.shallow(<MenuComponent menus={fake}/>);
+  it('should have correct number of buttons', () => {
+    expect(wrapper.is('div')).toEqual(true);
+    expect(wrapper.find('button').length).toBe(2);
+  });
   it('should change state if button is pressed', () => {
     const dinnerButton = wrapper.find('button').at(0);
     const lunchButton = wrapper.find('button').at(1);
     dinnerButton.simulate('click');
     expect(wrapper.state().currentMenuName).toBe('Dinner');
+    lunchButton.simulate('click');
+    expect(wrapper.state().currentMenuName).toBe('Lunch');
   });
 });
