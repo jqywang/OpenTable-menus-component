385b5aa58c7153a5edfd70321ff23a86df2be572
Jeffrey Wang <jqywang.95@gmail.com>
2018-03-03 15:00:49 -0800

    made the buttons more responsive and hover

--- client/src/subgroupComponent.jsx
+++ client/src/subgroupComponent.jsx
@@ -5,7 +5,7 @@ const SubgroupComponent = ({name, subgroup}) => {
     return (
     <div className='subgroup'>
       <h3>{name}</h3>
-      <h4>{subgroup.subgroup_desc}</h4>
+      <p>{subgroup.subgroup_desc}</p>
       <div className='allEntries'>
       {
         subgroup.entries.map((entry) => {
