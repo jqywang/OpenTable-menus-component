e03e585d396698e61882765e6a6b7c4d0f30071a
Jeffrey Wang <jqywang.95@gmail.com>
2018-03-02 16:38:58 -0800

    finished the css portion of the frontend. next up is the collapsable menu

--- client/src/entryComponent.jsx
+++ client/src/entryComponent.jsx
@@ -2,16 +2,18 @@ import React from 'react';
 
 const EntryComponent = ({entry}) => {
   return (
-  <div className ="entry">
+  <span className='entryContainer'>
     <div className='entryInfo'>
       <div className="name">{entry.name}</div>
       <div className="price">{entry.price}</div>
     </div>
+    <div className='clarity'> </div>
+    <p className='entryDesc'> {entry.desc} </p>
     <div className="extras">
       <div className="extrasName">{entry.extras.name}</div>
       <div className="extrasPrice">{entry.extras.price}</div>
     </div>
-  </div>
+  </span>
   );
 };
 
